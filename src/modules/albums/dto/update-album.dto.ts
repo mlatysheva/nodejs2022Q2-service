fa19458d9a4600fbf43a8385247476ea3b5e269c
mlatysheva <mlatysheva@gmail.com>
2022-07-15 23:05:05 +0200

    docs: update swagger doc

--- src/modules/albums/dto/update-album.dto.ts
+++ src/modules/albums/dto/update-album.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAlbumDto } from './create-album.dto';
-import { IsString } from 'class-validator';
 
 export class UpdateAlbumDto extends PartialType(CreateAlbumDto) {}
