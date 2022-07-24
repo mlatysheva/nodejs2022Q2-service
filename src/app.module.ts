5f71b6bba5606a6320546d10035972dfd2930d86
mlatysheva <mlatysheva@gmail.com>
2022-07-24 14:49:33 +0200

    feat: update prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,21 @@ import { FavoritesModule } from './modules/favorites/favorites.module';
 import { TracksModule } from './modules/tracks/tracks.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
     ArtistsModule,
     AlbumsModule,
     TracksModule,
     FavoritesModule,
     AuthModule,
+    PrismaModule,
   ],
   controllers: [],
   providers: [],
